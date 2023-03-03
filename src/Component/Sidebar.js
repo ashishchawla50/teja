28801c2495b4428012b2d7ac403b10d6ac58267e
Ashish Chawla <ashish.chawla50@gmail.com>
2023-03-03 21:22:16 +0530

    logo added, sidebar css fixed

--- src/Component/Sidebar.js
+++ src/Component/Sidebar.js
@@ -1,12 +1,23 @@
 import React from "react";
-
+import { logo } from "../Constant";
 function Sidebar(props) {
   return (
     <>
       <div className="sideBar">
+        <div className="logo">
+          <img src={logo} />
+        </div>
         <ul className="sideBarMenu">
-          <li>Daashboard</li>
-          <li>Feedback</li>
+          <li>
+            <span className="menuLabel">
+              <div className="menuLabelInner">Dashboard</div>{" "}
+            </span>
+          </li>
+          <li>
+            <span className="menuLabel">
+              <div className="menuLabelInner">Feedback</div>
+            </span>
+          </li>
         </ul>
       </div>
     </>
